2e21207fa22f5f2c610b8511803a31bef8420bdd
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2025-03-17 21:43:51 +0700

    adding abort controller on searching fetch

--- client/src/components/chat/ContactList.jsx
+++ client/src/components/chat/ContactList.jsx
@@ -1,15 +1,22 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { userAPI } from '../../constants';
 import { useFetch } from '../../hooks/useFetch';
 import SearchBar from './SearchBar';
 
 export default function ContactList() {
   const { error, isLoading, sendRequest } = useFetch();
+  const abortControllerRef = useRef(null);
 
   const handleSearch = (query) => {
     if (!query.trim()) {
       return;
     }
+
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+
+    abortControllerRef.current = new AbortController();
 
     console.log('Search query:', query);
 
@@ -17,12 +24,21 @@ export default function ContactList() {
       method: 'GET',
       url: userAPI.searchUser,
       params: { q: query },
+      signal: abortControllerRef.current.signal,
     };
 
     sendRequest(searchRequestConfig, (data) => {
       console.log('Search results:', data);
     });
   };
+
+  useEffect(() => {
+    return () => {
+      if (abortControllerRef.current) {
+        abortControllerRef.current.abort();
+      }
+    };
+  }, []);
 
   return (
     <div className="w-[400px] h-[80vh] bg-white shadow-2xl rounded-2xl p-4">
